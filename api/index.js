2a275f8669139c8ea6519a463284f037cdbe1ef8
Kishan-Jaiswar <jaiswarkishan78@gmail.com>
2025-04-03 00:19:01 +0530

    test

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ const app = express();
 const PORT = 4000;
 
 app.get("/home", (req, res) => {
-  res.status(200).json("Welcome, your app is working well");
+  res.status(200).json({ message: "Welcome, your app is working well" });
 });
 
 app.listen(PORT, () => {
